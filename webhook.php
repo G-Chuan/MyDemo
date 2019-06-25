0ae300ae9a8481deb99972c0fc76a070389321b4
G-Chuan <gao_chuan@foxmail.com>
2019-06-25 23:31:31 +0800

    new submit

--- webhook.php
+++ webhook.php
@@ -1,6 +1,6 @@
 <?php
-$secret = "mrzhouxiaofei";  //密钥，和 GitHub 上对应
-$path = "/var/www/html/Webhooks";  //服务器上的项目文件目录
+$secret = "github@mydemo";  //密钥，和 GitHub 上对应
+$path = "/usr/share/nginx/www/MyDemo";  //服务器上的项目文件目录
 $signature = $_SERVER["HTTP_X_HUB_SIGNATURE"];  //获取散列字符串
 if($signature) {
     $rawPost = file_get_contents("php://input");  //获取收到的数据
